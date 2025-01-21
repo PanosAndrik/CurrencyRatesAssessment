3549ee1d67717a260228c1f53e9ad6a213f88f94
PanosAndrik <katholoucrew@gmail.com>
2025-01-21 17:32:57 +0200

    wallet.cs created and added in AppDbContext.cs

--- CurrencyRatesApp/AppDbContext.cs
+++ CurrencyRatesApp/AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public DbSet<CurrencyRate> CurrencyRates { get; set; }
+    public DbSet<Wallet> Wallets { get; set; } // Add Wallet for 3rd task
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -10,10 +11,12 @@ public class AppDbContext : DbContext
     }
 }
 
-[Index(nameof(Currency), IsUnique = true)] // Add unique index to Currency
+[Index(nameof(Currency), IsUnique = true)]
 public class CurrencyRate
 {
     public int Id { get; set; }
     public string? Currency { get; set; }
     public decimal Rate { get; set; }
 }
+
+
---
+++ CurrencyRatesApp/Wallet.cs
@@ -0,0 +1,11 @@
+public class Wallet
+{
+    public long Id { get; set; } // Id: long 
+    public decimal Balance { get; set; } // Balance: decimal
+    public string Currency { get; set; } // Currency: string 
+}
+
+
+// Id: long (the unique identifier of the wallet)
+// Balance: decimal (the available balance in the wallet)
+// Currency: string (the currency in which the wallet balance is maintained)
